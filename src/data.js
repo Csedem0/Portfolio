452657e8a3b409cda3cca16a91300e3578b4a927
Emmanuel Chukwu <emmasobula@gmail.com>
2025-05-25 16:48:14 +0100

    Correct links all added

--- src/data.js
+++ src/data.js
@@ -6,19 +6,19 @@ export const products = [
     },
     {
         id: 2,
-        img: "https://img.freepik.com/free-photo/programming-background-with-person-working-with-codes-computer_23-2150010125.jpg?semt=ais_hybrid&w=740",
+        img: "https://i.postimg.cc/htPn8h7f/Swap-Desk.png",
         link: "http://www.swapdesk.ng",
     },
     {
         id: 3,
-        img: "https://drive.google.com/file/d/15IcX7bn2qP15nr-24Nde348w9RximdNd/view?usp=sharing",
+        img: "https://i.postimg.cc/kGnKMxBn/Trash-Express.png",
         link: "https://trashexpress.ng/",
     },
-    {
-        id: 4,
-        img: "https://fiverr-res.cloudinary.com/image/upload/w_880/f_auto,q_auto/v1/attachments/generic_asset/asset/767bc10a0c44b0a67be1a94aea270205-1609336032121/website%20development-min.jpg",
-        link: "https://appleprediction.onrender.com/",
-    },
+    // {
+    //     id: 4,
+    //     img: "https://fiverr-res.cloudinary.com/image/upload/w_880/f_auto,q_auto/v1/attachments/generic_asset/asset/767bc10a0c44b0a67be1a94aea270205-1609336032121/website%20development-min.jpg",
+    //     link: "https://appleprediction.onrender.com/",
+    // },
     // {
     //     id: 5,
     //     img: "https://i.ibb.co/CHJ7G28/screencapture-localhost-8888-notebooks-data-science-portfolio-1-ipynb-2024-05-15-23-07-14.png",
